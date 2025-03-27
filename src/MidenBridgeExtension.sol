dd82986d4c0ae88f7327c525eebda232cd51e088
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2025-03-27 10:22:40 +0300

    feat: evm side contracts

---
+++ src/MidenBridgeExtension.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@zkevm/v2/PolygonZkEVMBridgeV2.sol";
+import {IBridgeAndCall} from "@lxly/IBridgeAndCall.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
+error MalformedRecipientData();
+error CallAddressShouldBeZero();
+error FallbackAddressShouldBeZero();
+error DestinationNetworkIsNotTheMidenChain();
+error AmountDoesNotMatchMsgValue();
+error InvalidAddress();
+error InvalidDepositIndex();
+error OriginMustBeBridgeExtension();
+error SenderMustBeBridge();
+error UnclaimedAsset();
+
+contract MidenBridgeExtension is IBridgeAndCall {
+    using SafeERC20 for IERC20;
+
+    uint32 constant public MIDEN_NETWORK_ID = 9966;
+
+    PolygonZkEVMBridgeV2 public immutable bridge;
+    address public immutable midenBridgeAddress;
+    
+    constructor(address bridge_, bytes15 midenBridge_) {
+        bridge = PolygonZkEVMBridgeV2(bridge_);
+        midenBridgeAddress = address(bytes20(midenBridge_));
+    }
+    
+    function bridgeAndCall(
+        address token,
+        uint256 amount,
+        uint32 destinationNetwork,
+        address callAddress,
+        address fallbackAddress,
+        bytes calldata callData,
+        bool forceUpdateGlobalExitRoot
+    ) external payable {
+        _ensureRecipientDataValidity(callData);
+        if (callAddress != address(0)) revert CallAddressShouldBeZero();
+        if (fallbackAddress != address(0)) revert FallbackAddressShouldBeZero();
+        if (destinationNetwork != MIDEN_NETWORK_ID) revert DestinationNetworkIsNotTheMidenChain();
+
+        uint256 dependsOnIndex = bridge.depositCount() + 1; // only doing 1 bridge asset
+
+        if (token != address(0) && token == address(bridge.WETHToken())) {
+            // user is bridging ERC20 (WETH)
+            uint256 balanceBefore = IERC20(token).balanceOf(address(this)); // WETH will only be taxable if it's modified by the chain operator
+
+            // transfer assets from caller to this extension
+            IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+
+            uint256 balanceAfter = IERC20(token).balanceOf(address(this));
+            amount = balanceAfter - balanceBefore;
+
+            // transfer the erc20 - using a helper to get rid of stack too deep
+            _bridgeNativeWETHAssetHelper(
+                token, amount, destinationNetwork
+            );
+        } else if (token == address(0)) {
+            // user is bridging the gas token
+            if (msg.value != amount) {
+                revert AmountDoesNotMatchMsgValue();
+            }
+
+            // transfer native gas token (e.g. eth) - using a helper to get rid of stack too deep
+            _bridgeNativeAssetHelper(amount, destinationNetwork);
+        } else {
+            // user is bridging ERC20 - beware of tax tokens
+            uint256 balanceBefore = IERC20(token).balanceOf(address(this));
+
+            // transfer assets from caller to this extension
+            IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+
+            uint256 balanceAfter = IERC20(token).balanceOf(address(this));
+            amount = balanceAfter - balanceBefore;
+
+            // transfer the erc20 - using a helper to get rid of stack too deep
+            _bridgeERC20AssetHelper(
+                token, amount, destinationNetwork
+            );
+        }
+
+        // assert that the index is correct - avoid any potential reentrancy caused by bridgeAsset
+        if (dependsOnIndex != bridge.depositCount()) revert InvalidDepositIndex();
+
+        bytes memory encodedMsg;
+        if (token != address(0) && token == address(bridge.WETHToken())) {
+            // WETHs originNetwork is always 0 as it is a special case
+            encodedMsg =
+                abi.encode(dependsOnIndex, 0, address(0), callData);
+        } else if (token == address(0)) {
+            // bridge the message (which gets encoded with extra data) to the extension on the destination network
+            encodedMsg = abi.encode(
+                dependsOnIndex,
+                bridge.gasTokenNetwork(),
+                bridge.gasTokenAddress(),
+                callData
+            );
+        } else {
+            // we need to encode the correct token network/address
+            (uint32 assetOriginalNetwork, address assetOriginalAddr) = bridge.wrappedTokenToTokenInfo(token);
+            if (assetOriginalAddr == address(0)) {
+                // only do this when the token is from this network
+                assetOriginalNetwork = bridge.networkID();
+                assetOriginalAddr = token;
+            }
+
+            // bridge the message (which gets encoded with extra data) to the extension on the destination network
+            encodedMsg = abi.encode(
+                dependsOnIndex, assetOriginalNetwork, assetOriginalAddr, callData
+            );
+        }
+
+        bridge.bridgeMessage(destinationNetwork, midenBridgeAddress, forceUpdateGlobalExitRoot, encodedMsg);
+
+    }
+
+    function _ensureRecipientDataValidity(bytes calldata callData) internal pure {
+        if (callData.length != 32) revert MalformedRecipientData();
+    }
+
+    function _bridgeNativeWETHAssetHelper(
+        address token,
+        uint256 amount,
+        uint32 destinationNetwork
+    ) internal {
+        // bridge the ERC20 assets - no need to approve, bridge will burn the tokens
+        bridge.bridgeAsset(destinationNetwork, midenBridgeAddress, amount, token, false, "");
+    }
+
+    function _bridgeNativeAssetHelper(
+        uint256 amount,
+        uint32 destinationNetwork
+    ) internal {
+
+        // bridge the native assets
+        bridge.bridgeAsset{value: amount}(destinationNetwork, midenBridgeAddress, amount, address(0), false, "");
+    }
+
+    function _bridgeERC20AssetHelper(
+        address token,
+        uint256 amount,
+        uint32 destinationNetwork
+    ) internal {
+
+        {
+            // we need to encode the correct token network/address
+            (uint32 assetOriginalNetwork, address assetOriginalAddr) = bridge.wrappedTokenToTokenInfo(token);
+            if (assetOriginalAddr == address(0)) {
+                // only do this when the token is from this network
+                assetOriginalNetwork = bridge.networkID();
+                assetOriginalAddr = token;
+
+                // allow the bridge to take the assets when needed - in the other scenarios the token is a wrapper that is burned
+                IERC20(token).approve(address(bridge), amount);
+            }
+        }
+
+        // bridge the ERC20 assets
+        bridge.bridgeAsset(destinationNetwork, midenBridgeAddress, amount, token, false, "");
+    }
+
+    
+}
